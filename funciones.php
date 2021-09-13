302409a174739d4fd8c71effaa2ec2654fd4b23d
DevPed <pedromfr91@gmail.com>
2021-09-13 07:56:46 +0200

    ShowProfile

--- funciones.php
+++ funciones.php
@@ -38,3 +38,16 @@ function sanitizeString($var)
     }
     return $connection->real_escape_string($var);
 }
+function showProfile($user){
+    if(file_exists("$user.jpg"))
+    {
+        echo "<img src='$user.jpg' style='float:left;'>";
+    }
+    $result=queryMysql("SELECT * FROM profiles WHERE user='$user'");
+    if($result->num_rows)
+    {
+        $row=$result->fetch_array(MYSQLI_ASSOC);
+        echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    }
+    else echo "<p>Nada que ver aún.</p><br>";
+}
