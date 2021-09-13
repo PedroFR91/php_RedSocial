e9fd9a222c645106efd887c2231b9c7b81fd50f2
DevPed <pedromfr91@gmail.com>
2021-09-13 07:27:14 +0200

    CreateTable

--- funciones.php
+++ funciones.php
@@ -5,3 +5,10 @@ $dbuser='root';
 $dbpass='123456';
 
 $connection= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+if($connection->connect_error) die("Fatal error");
+
+function createTable($name,$query)
+{
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Tabla '$name' creada o existente<br>";
+} 
